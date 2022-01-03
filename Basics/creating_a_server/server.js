b385bdf52a4cf0d229f0ed9bc15ce0740c4229ad
jaskirat singh <singhjaskirat984@gmail.com>
2022-01-04 00:43:55 +0530

    redirecting requests

--- Basics/creating_a_server/server.js
+++ Basics/creating_a_server/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const http = require('http');
 
 
@@ -21,6 +22,29 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     // process.exit();
+    // === means string and contains
+    var url=req.url;
+    var method=req.method;
+    // routing requests
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        // return used here in this line bcoz we do't want to run 
+        // the line which are outside this if
+        return res.end();
+    }
+
+    // Redirecting requests
+    if(url === '/message' && method==='POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode=302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+
     res.setHeader('content-type','text/html');
     res.write('<html>');
     res.write('<head><title>My firts page</title><head>');
