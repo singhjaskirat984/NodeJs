670be7f98fbd1c3c9a0d4bcdde46545ae1d064fe
Jaskirat Singh <95342622+jaskiratAtNexG@users.noreply.github.com>
2022-01-03 19:01:04 +0530

    send responses

--- Basics/creating_a_server/server.js
+++ Basics/creating_a_server/server.js
@@ -19,7 +19,14 @@ const http = require('http');
 // or
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My firts page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
